9b7cebb6368b3d4d9deefbb0e8f137fd8353d8e8
peppe-99 <g.cardaropoli99@gmail.com>
2021-07-31 13:06:46 +0200

    Aggiunti parametri di testing e commenti sui dati raccolti durante il training

--- utils/parameters.py
+++ utils/parameters.py
@@ -2,7 +2,7 @@ import torch
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-img_size = 128      # dimensione delle immagini
+img_size = 128      # dimensione delle immagini (64 x 64)
 epochs = 100        # numero di epoche del training
 batch_size = 64     # grandezza di un batch di campioni
 lr = 0.0002         # tasso di apprendimento
@@ -14,6 +14,37 @@ label = torch.FloatTensor(batch_size, 1, 1, 1).cuda().to(device)              # 
 real_label = 1
 fake_label = 0
 
-TRAIN_RESULT = 'risultati/training/'    # Directory risultati training
+losses_discriminatore = []
+losses_generatore = []
 
+real_scores = []
+fake_scores = []
+
+TRAIN_RESULT = 'risultati/training/'    # Directory risultati training
+TEST_RESULT = 'risultati/testing'       # Directory risultati testing
+
+"""
+    x rappresenta un immagine di dimensione 3 x 64 x 64
+    
+    D(x) è il risultato del discriminatore sull'input x. Rappresenta la probabbilità che x sia 
+    reale piuttosto che generata. D(x) dovrebbe essere alto quando x è reale e basso se è generato.
+    Sostanzialmente D(x) e il discriminatore stesso sono un semplice classificatore binario.
+    
+    z è un vettore dello spazio latente campionato da una distribuzione normale standard.
+    
+    G(z) è la funzione generatrice che mappa il vettore latente z nello spazio dati. L'obiettivo della funzione G è
+    quello di stimare la distribuzione da cui provengono i dati di allenamento (p_data) in modo da poter generare
+    campioni falsi da quella disatribuzione stimata (p_g). Sostanzialmente l'obiettivo del generatore è quello di 
+    generare nuove immagini cercando di confonderle tra quelle reali.
+    
+    D(G(z)) è la probabbilità che l'output del generatore G sia un immagine reale. 
+    
+    Sostanzialmente D e G giocano al gioco del minimax dove D cerca di massimizzare la probabbilità di classificare
+    correttamente (accuratezza) le immagini reali e false [ log(D(x)) ]. Invece, G prova a minimizzare la probbabilità
+    che D classifichi i suoi output come falsi [log(1 - D(G(z)))].
+     
+    Il gioco si risolve quando p_g = p_data ovvero immagini generate e reali combaciano ed il discriminatore sarà
+    costretto a classificarle casualmente.
+    
+"""
 

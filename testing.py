6713f73c678dc142764ae726d8dc9fb65f84a38a
peppe-99 <g.cardaropoli99@gmail.com>
2021-07-31 13:10:49 +0200

    Testing

--- testing.py
+++ testing.py
@@ -1,12 +1,14 @@
 from model.generator import Generator
 from model.discriminator import Discriminator
-from utils.function import prepare_data, ritagliare_centro
+from utils.function import prepare_data, ritagliare_centro, create_dir
 from utils.parameters import *
 
 import torchvision.utils as vutils
 
-
+from tqdm import tqdm
 import torch
+
+create_dir(TEST_RESULT)
 
 generator = Generator()
 generator.load_state_dict(torch.load("./log/generator.pt"))
@@ -18,7 +20,9 @@ discriminator.eval()
 
 dataloader = prepare_data("./dataset/testing/")
 
-for i, data in enumerate(dataloader, 0):
+i = 1
+
+for data in tqdm(dataloader, ncols=100, desc="Batch analizzati"):
     real_cpu, _ = data
     real_center_cpu = real_cpu[:, :, int(img_size / 4):int(img_size / 4) + int(img_size / 2),
                       int(img_size / 4):int(img_size / 4) + int(img_size / 2)]
@@ -32,11 +36,12 @@ for i, data in enumerate(dataloader, 0):
     input_real, input_cropped, real_center = ritagliare_centro(input_real, input_cropped, real_cpu, real_center,
                                                                real_center_cpu)
 
-    fake = generator(input_real)
+    fake = generator(input_cropped)
 
     recon_image = input_cropped.clone()
     recon_image.data[:, :, int(img_size / 4):int(img_size / 4 + img_size / 2),
     int(img_size / 4):int(img_size / 4 + img_size / 2)] = fake.data
 
-    vutils.save_image(recon_image, './ricostruite_testing.png')
-    break
+    vutils.save_image(recon_image, TEST_RESULT + f"/ricostruite_testing_{i}.png")
+
+    i += 1
